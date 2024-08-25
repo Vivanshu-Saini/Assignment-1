01607564b30a638a7b7087c9c5e52725fac0ed2f
Vivanshu-Saini <every.anime.03@gmail.com>
2024-08-25 18:27:25 +0530

    Update Q1.py

--- Q1.py
+++ Q1.py
@@ -1,10 +1,1 @@
-def add_numbers(num1, num2):
-    # Return the sum of the two numbers
-    return num1 + num2;
-
-# Example usage of the function
-result = add_numbers(5, 3)
-print("The sum is:", result)
-
-
 
